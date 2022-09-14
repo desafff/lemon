90bdb1c79c679290144a85250d0ebbb3d6e220a7
ferasa <86012372+desafff@users.noreply.github.com>
2022-09-14 08:18:32 +0700

    Update processhider.c

--- processhider.c
+++ processhider.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "enbem";
+static const char* process_to_filter = "luln";
 
 /*
  * Get a directory name given a DIR* handle
